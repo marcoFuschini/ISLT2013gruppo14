dc1e30408d165ce23c6faaaa86b4d47b9561b8fb
marco <marco.fuschini@gmail.com>
2015-02-05 16:40:57 +0100

    Signed-off-by: marco <marco.fuschini@gmail.com>

---
+++ FanDeviceProj/src/it/unibo/IngSW/FanDevice/Fan.java
@@ -0,0 +1,34 @@
+package it.unibo.IngSW.FanDevice;
+
+/**
+ * @author Fabio
+ * @version 1.0
+ * @created 05-feb-2015 16.06.26
+ */
+public class Fan implements IActuator {
+
+	public Fan(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void decSpeed(){
+
+	}
+
+	public void incSpeed(){
+
+	}
+
+	/**
+	 * 
+	 * @param speed
+	 */
+	public void setSpeed(FanSpeed speed){
+
+	}
+
+}
---
+++ FanDeviceProj/src/it/unibo/IngSW/FanDevice/IActuator.java
@@ -0,0 +1,20 @@
+package it.unibo.IngSW.FanDevice;
+
+/**
+ * @author Fabio
+ * @version 1.0
+ * @created 05-feb-2015 16.06.26
+ */
+public interface IActuator {
+
+	public void decSpeed();
+
+	public void incSpeed();
+
+	/**
+	 * 
+	 * @param speed
+	 */
+	public void setSpeed(FanSpeed speed);
+
+}
