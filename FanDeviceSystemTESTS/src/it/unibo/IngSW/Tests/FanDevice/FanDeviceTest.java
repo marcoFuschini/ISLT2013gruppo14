79c1d161033ffddfff6740149195063eb1df5c1f
marco <marco.fuschini@gmail.com>
2015-02-05 15:36:33 +0100

    Aggiunto file test per FanDevice

---
+++ FanDeviceSystemTESTS/src/it/unibo/IngSW/Tests/FanDevice/FanDeviceTest.java
@@ -0,0 +1,61 @@
+package it.unibo.IngSW.Tests.FanDevice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FanDeviceTest {
+
+	@Test
+	public void testAddSensor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecSpeed() {
+		Object l=null;
+		assertNull(l);
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReceiveCommand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReceiveSensorData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveSensor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+}
