8ef97c69ed6799039db0a624283b7b4fe04d9086
Fabio <Fabio@Fabio-PC>
2015-02-08 17:57:44 +0100

    Test

--- FanDeviceSystemTESTS/src/it/unibo/IngSW/Tests/CommonClasses/ClientComunicatorTest.java
+++ FanDeviceSystemTESTS/src/it/unibo/IngSW/Tests/CommonClasses/ClientComunicatorTest.java
@@ -1,43 +1,34 @@
 package it.unibo.IngSW.Tests.CommonClasses;
 
 import static org.junit.Assert.*;
-import it.unibo.IngSW.FanDevice.ServerComunicator;
-import it.unibo.IngSW.common.ClientComunicator;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ClientComunicatorTest {
 
-	private ClientComunicator client;
-	private ServerComunicator server;
 	@BeforeClass
-	public void init(){
-		client=new ClientComunicator();
-		server=new ServerComunicator();
+	public static void setUpBeforeClass() throws Exception {
 	}
-	
+
 	@Test
 	public void testConnect() {
-		server.connect("server");		
-		assertTrue(client.connect("127.0.0.1"));
-	}
-
-	@Test
-	public void testReadData() {
-		server.writeData("data");
-		assertEquals("data", client.readData());
-	}
-
-	@Test
-	public void testWriteData() {
-		client.writeData("example");
-		assertEquals("data",server.readData());
+		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDisconnect() {
-		assertTrue(client.disconnect());
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteData() {
+		fail("Not yet implemented");
 	}
 
 }
