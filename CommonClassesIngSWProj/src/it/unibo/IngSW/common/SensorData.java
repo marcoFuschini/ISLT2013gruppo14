061f08b04ad68796bd91cb505aa0fced212b7a54
Marco <Marco@MarcoFisso>
2015-02-24 18:31:33 +0100

    Signed-off-by: Marco <Marco@MarcoFisso>

--- CommonClassesIngSWProj/src/it/unibo/IngSW/common/SensorData.java
+++ CommonClassesIngSWProj/src/it/unibo/IngSW/common/SensorData.java
@@ -42,4 +42,35 @@ public class SensorData implements ISensorData {
 		this.val=value;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((val == null) ? 0 : val.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SensorData other = (SensorData) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (val == null) {
+			if (other.val != null)
+				return false;
+		} else if (!val.equals(other.val))
+			return false;
+		return true;
+	}
+
 }
