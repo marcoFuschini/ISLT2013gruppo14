4d45739015e26d7fd08399877a9297e99e873400
Marco <Marco@MarcoFisso>
2015-02-24 19:00:26 +0100

    spariti main viewer e controlunit
    Signed-off-by: Marco <Marco@MarcoFisso>

--- ControlUnitProj/src/it/unibo/IngSW/ControlUnit/ControlUnitMain.java
+++ ControlUnitProj/src/it/unibo/IngSW/ControlUnit/ControlUnitMain.java
@@ -30,121 +30,14 @@ import it.unibo.IngSWBasicComponents.VirtualLed;
  */
 public class ControlUnitMain {
 
-	private static IControlUnit controlUnit;
-	private static Display display;
-	private static PhysicalButton[] pButtons = { new PhysicalButton(' ') };
-	private static IButton[] buttons = { new VirtualButton("Start"),
-			new HybridButton("Stop", pButtons[0]), new VirtualButton("LOW"),
-			new VirtualButton("MID"), new VirtualButton("HIGH"),
-			new VirtualButton("decrement speed"), 
-			new VirtualButton("increment speed")};
-	private static IElementDisplay[] displayEls = { new VirtualLed(),
-			new TachometerDisplay(), new ThermometerDisplay() };
 
-	private static JPanel mainPane;
-	private static boolean inputSenderRun = true,datReceiverRun=true;
 
-	/**
-	 * 
-	 * @param args
-	 */
-	private static void creaGUI() {
-		// IMPLEMENTAZIONE SPECIFICA
-
-		// registro i bottoni fisici in modo da ascoltare le pressioni dei tasti
-		// sul frame
-		for (PhysicalButton pb : pButtons) {
-			display.addKeyListener(pb);
-		}
-
-		// inserisco i componenti grafici nel panel
-		mainPane = new JPanel(new GridBagLayout());
-		GridBagConstraints c;
-		c = new GridBagConstraints();
-		c.weightx = 1;
-		c.gridx = 0;
-		c.gridy = 0;
-
-		mainPane.add((Component) displayEls[0], c);// led
-		c.gridx++;
-		mainPane.add((JButton) buttons[0], c);// start
-		c.gridx++;
-		mainPane.add((JButton) buttons[1], c);// stop
-		c.gridx = 0;
-		c.gridy++;
-		mainPane.add((JButton) buttons[2], c);// low
-		c.gridx++;
-		mainPane.add((JButton) buttons[3], c);// mid
-		c.gridx++;
-		mainPane.add((JButton) buttons[4], c);// high
-		c.gridx = 0;
-		c.gridy++;
-		mainPane.add((JButton) buttons[5], c);// dec
-		c.gridx++;
-		mainPane.add((JButton) buttons[6], c);// inc
-		c.gridx = 0;
-		c.gridy++;
-		c.gridy++;
-
-		mainPane.add((Component) displayEls[1], c);// Speed
-		c.gridx++;
-		mainPane.add((Component) displayEls[2], c);// Temperature
-		c.gridx++;
-
-		display.setContentPane(mainPane);
-		display.setResizable(false);
-		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		display.pack();
-		display.setVisible(true);
-
-	}
+	
 
 	public static void main(String[] args) {
-		// init
-		display = new Display(displayEls);
-		controlUnit = new ControlUnit(display, buttons);
-		creaGUI();
-		Thread inputSenderT = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				String cmd;
-				String json;
-				do {
-
-					try {
-						cmd = controlUnit.readCommand();
-						json = JSONConverter.commandToJSON(cmd);
-						controlUnit.sendCommand(json);
-					} catch (Exception e) {
-						kill();
-						e.printStackTrace();
-					}
-				} while (inputSenderRun);
-			}
-		});
 		
-		ISensorData[] data;
-		do{
-			try{
-			data=controlUnit.receiveData();
-			controlUnit.updateData(data);
-			}catch (Exception e){
-				kill();
-				e.printStackTrace();
-			}
-		}while(datReceiverRun&&display.isVisible());
-		kill();
 	}
 	
-	private static void kill(){
-		inputSenderRun=false;
-		datReceiverRun=false;
-		try {
-			controlUnit.disconnect();
-		} catch (Exception e) {
-		}
-		display.dispose();
-	}
+	
 
 }
--- ViewerProj/src/it/unibo/IngSW/Viewer/ViewerMain.java
+++ ViewerProj/src/it/unibo/IngSW/Viewer/ViewerMain.java
@@ -24,90 +24,9 @@ import javax.swing.JPanel;
  */
 public class ViewerMain {
 
-	private static final int MAXCOLS = 5;
-
-	private static IViewer viewer;
-	private static JPanel mainPane;
-	private static Display display;
-	private static IElementDisplay[] displayEls = {new TachometerDisplay(),new ThermometerDisplay()};
-
-	private static void scrivi(String s){
-		System.out.println(s);
-	}
-	private static void creaGUI() {
-		// IMPLEMENTAZIONE SPECIFICA
-
-		mainPane = new JPanel(new GridBagLayout());
-		GridBagConstraints c;
-		c = new GridBagConstraints();
-		c.weightx = 10;
-		c.weighty = 10;
-		c.gridx = 0;		
-		c.gridy = 0;
-		c.ipadx = 50;
-		c.ipady = 10;
-
-		boolean stop = false;
-		int y=0;
-		while (!stop) {
-			for (int x = 0; x < MAXCOLS; x++) {
-				if(displayEls.length<=(x+y*MAXCOLS)){
-					stop=true;
-					break;
-				}
-				c.gridx=x;
-				c.gridy=y;
-				mainPane.add((Component) displayEls[(x+y*MAXCOLS)],c);
-			}
-			y++;
-		}
-
-		display.setContentPane(mainPane);
-		display.setResizable(false);
-		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		display.pack();
-		display.setVisible(true);
-	}
 	
 	public static void main(String[] args) {
-		display=new Display(displayEls);
-		viewer = new Viewer(display);
-		creaGUI();
 		
-		try {
-			viewer.connect(args[0],Integer.parseInt(args[1]));
-		} catch (NumberFormatException e) {
-			scrivi("Porta non valida");
-			e.printStackTrace();
-			return;
-		} catch (Exception e) {
-			scrivi("Connection timeout");
-			e.printStackTrace();
-			esci();
-		}
-		ISensorData[] data;
-		while(display.isVisible()){
-			try {
-				data=viewer.receiveData();
-				viewer.updateData(data);
-			} catch (Exception e) {
-				scrivi("Receive data error");
-				e.printStackTrace();
-				esci();
-			}			
-		}
-			try {
-				viewer.diconnect();
-			} catch (Exception e) {
-				scrivi("Disconnection error");
-				e.printStackTrace();
-			}
-			esci();//mi assicuro che venga chiusa l'interfaccia grafica a seguito di errori
-	}
-
-	public static void esci(){
-		display.dispose();
-		System.exit(0);
 	}
 
 }
