c49a5fb0ac5faef163c92a0126768d03ea0508d9
Marco <Marco@MarcoFisso>
2015-02-25 16:22:49 +0100

    Signed-off-by: Marco <Marco@MarcoFisso>

--- CommonClassesIngSWProj/src/it/unibo/IngSW/common/Command.java
+++ CommonClassesIngSWProj/src/it/unibo/IngSW/common/Command.java
@@ -2,4 +2,26 @@ package it.unibo.IngSW.common;
 
 public enum Command {
 	START,STOP,DECSPEED,INCSPEED,LOWSPEED,MEDIUMSPEED,HIGHSPEED;
+	
+	
+	public static Command StringToCommand(String s){
+		
+		if(START.toString().equals(s)){
+			return START;
+		}else if(STOP.toString().equals(s)){
+			return STOP;
+		}else if(DECSPEED.toString().equals(s)){
+			return DECSPEED;
+		}else if(INCSPEED.toString().equals(s)){
+			return INCSPEED;
+		}else if(LOWSPEED.toString().equals(s)){
+			return LOWSPEED;
+		}else if(MEDIUMSPEED.toString().equals(s)){
+			return MEDIUMSPEED;
+		}else if(HIGHSPEED.toString().equals(s)){
+			return HIGHSPEED;
+		}
+		return null;
+	}
 }
+
